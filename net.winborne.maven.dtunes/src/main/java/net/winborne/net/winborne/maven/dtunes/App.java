8c7522028b7f44e9640abef99622304d6fe672af
William Winborne <will@winborne.net>
2023-01-07 00:08:09 -0500

    - place youtube-dl.exe from internal resources when it is needed

--- net.winborne.maven.dtunes/src/main/java/net/winborne/net/winborne/maven/dtunes/App.java
+++ net.winborne.maven.dtunes/src/main/java/net/winborne/net/winborne/maven/dtunes/App.java
@@ -1,12 +1,29 @@
 package net.winborne.net.winborne.maven.dtunes;
 
 import java.awt.EventQueue;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.StandardCopyOption;
 
 /**
  * Entry point of DTunes. Created by William Winborne 12/4/2022.
  */
 public class App {
 	public static void main(String[] args) {
+		
+		// plant a copy of youtube-dl
+		ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+		InputStream is = classloader.getResourceAsStream("youtube-dl.exe");
+		File targetFile = new File("youtube-dl.exe");
+		
+		try {
+			java.nio.file.Files.copy(is, targetFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
