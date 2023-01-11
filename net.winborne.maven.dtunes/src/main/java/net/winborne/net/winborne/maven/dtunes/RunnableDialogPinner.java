c1e2f553a98ad7794b04f6356c2194eee700a084
William Winborne <will@winborne.net>
2023-01-10 23:18:28 -0500

    - remove unused imports

--- net.winborne.maven.dtunes/src/main/java/net/winborne/net/winborne/maven/dtunes/RunnableDialogPinner.java
+++ net.winborne.maven.dtunes/src/main/java/net/winborne/net/winborne/maven/dtunes/RunnableDialogPinner.java
@@ -1,11 +1,5 @@
 package net.winborne.net.winborne.maven.dtunes;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-
 public class RunnableDialogPinner implements Runnable {
 
 	@Override
